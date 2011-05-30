b6d4f2962a63f5810070568f1891d1e9bcc2063c
Mathias Gottschlag <mgottschlag@gmail.com>
2011-05-30 23:01:17 +0200

    Added first mostly empty source files and implemented a CMake build system.

---
+++ ddcn_service/JobAdaptor.h
@@ -0,0 +1,61 @@
+/*
+Copyright 2011 Benjamin Fus, Florian Muenchbach, Mathias Gottschlag. All
+rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+EVENT SHALL <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef JOBADAPTOR_H_INCLUDED
+#define JOBADAPTOR_H_INCLUDED
+
+#include "Job.h"
+
+#include <QDBusAdaptor>
+
+/**
+ * Class which exports a Job instance via D-Bus.
+ */
+class JobAdaptor : public QDBusAbstractAdaptor {
+	Q_OBJECT
+	Q_CLASSINFO("D-Bus Interface", "org.ddcn.Job")
+public:
+	/**
+	 * Creates an adaptor which exports a Job via D-Bus.
+	 * @param job Object to be exported.
+	 */
+	explicit JobAdaptor(Job *job);
+public slots:
+	/**
+	 * Aborts the job, causing it to finish with an error.
+	 */
+	void abort();
+public signals:
+	/**
+	 * Triggered when the job finishes.
+	 * @param success True if the job could be compiled successfully.
+	 * @param consoleOutput Output of the compiler command.
+	 */
+	void finished(bool success, QString consoleOutput);
+};
+
+#endif
+
